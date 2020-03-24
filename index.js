a1bf2be8344faf74b727f2cd520fd3cccaf0e326
MelvinManni <melvinnnamani@gmail.com>
2020-03-24 03:49:40 +0100

    little fixes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
-import App from './components/app';
+import App from './src/components/app';
 
 // var remove = document.getElementById("remove");
 // remove.addEventListener("click", ()=> remove.parentElement.style.display = 'none'
