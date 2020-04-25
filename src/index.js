03b7d2d87dae93e6ce24657e008dafcd97f2fb48
MelvinManni <melvinnnamani@gmail.com>
2020-04-25 08:16:54 +0100

    +++service worker

--- src/index.js
+++ src/index.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom'
 import App from './components/app';
-
-// var remove = document.getElementById("remove");
-// remove.addEventListener("click", ()=> remove.parentElement.style.display = 'none'
-// );
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     <div>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <React.StrictMode>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </React.StrictMode>
     </div>,
     document.getElementById('root'));
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
