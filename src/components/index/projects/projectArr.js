6ab14847fefcff119394b55abcbb8c9b4f56179e
MelvinManni <melvinnnamani@gmail.com>
2020-07-13 10:18:41 +0100

    cool-collage +++

--- src/components/index/projects/projectArr.js
+++ src/components/index/projects/projectArr.js
@@ -1,86 +1,94 @@
 let projectAr = [
+  {
+    id: 2,
+    src: './assets/images/projects/simple-calculator.png',
+    link: 'https://melvinmanni.github.io/Simple-Multiplier/',
+    title: 'Simple Multiplier',
+    description:
+      'Simple multiplier incoporates Javascript DOM to multiply two values keyed in by the user.',
+    tag: '#Calculation',
+    techs: 'JavaScript',
+  },
 
+  {
+    id: 5,
+    src: './assets/images/projects/agrow.png',
+    link: 'http://melvinmanni.github.io/Agrow-Farm/',
+    title: 'Agrow-Farm',
+    description: 'Agrow farm is a template site for an Agro based business',
+    tag: '#website',
+    techs: 'JavaScript Bootsrap',
+  },
 
-    {
-        id: 2,
-        src: "./assets/images/projects/simple-calculator.png",
-        link: "https://melvinmanni.github.io/Simple-Multiplier/",
-        title: "Simple Multiplier",
-        description: "Simple multiplier incoporates Javascript DOM to multiply two values keyed in by the user.",
-        tag: "#Calculation",
-        techs: "JavaScript",
-    },
+  {
+    id: 6,
+    src: './assets/images/projects/sync-blog.png',
+    link: 'https://syncblog.herokuapp.com/',
+    title: 'Sync Blog',
+    description:
+      'Sync blog is a project created in my quest to learn and be well versed with backend development and APIs. It is a dynamic Multipage Web application built with node.js and MongoDB to persist data.',
+    tag: 'Web-Application',
+    techs: 'Node.js Ejs',
+  },
 
-    {
-        id: 5,
-        src: "./assets/images/projects/agrow.png",
-        link: "http://melvinmanni.github.io/Agrow-Farm/",
-        title: "Agrow-Farm",
-        description: "Agrow farm is a template site for an Agro based business",
-        tag: "#website",
-        techs: "JavaScript Bootsrap",
-    },
+  {
+    id: 1,
+    link: 'https://melvinmanni.github.io/ionic-cgpa-calculator/',
+    src: './assets/images/projects/cgpa-calculator.png',
+    title: 'CGPA Calculator',
+    description:
+      'I have always found it a bit tedious calculating CGPA manually so i thought of a way to use what i love doing to make things a little easier for me. :)',
+    tag: 'Web-Application Cordova',
+    techs: 'Jquery Ionic',
+  },
 
-    {
-        id: 6,
-        src: "./assets/images/projects/sync-blog.png",
-        link: "https://syncblog.herokuapp.com/",
-        title: "Sync Blog",
-        description: "Sync blog is a project created in my quest to learn and be well versed with backend development and APIs. It is a dynamic Multipage Web application built with node.js and MongoDB to persist data.",
-        tag: "Web-Application",
-        techs: "Node.js Ejs",
-    },
+  {
+    id: 7,
+    src: './assets/images/projects/HugoFx.png',
+    link: 'https://melvinmanni.github.io/HUGO-s-Way/index.html',
+    title: 'HUGOfx',
+    description:
+      'HUGOfx is a dashboard template site, it incoporates editable table and fully responsive and comfy feel for both mobile and desktop',
+    tag: '#Website #Dashboard',
+    techs: 'JavaScript datatable.js',
+  },
 
+  {
+    id: 8,
+    src: './assets/images/projects/music-player.png',
+    link: 'https://melvinmanni.github.io/music-player',
+    title: 'Web Mp3 Player',
+    description:
+      'This is a static mp3 player site preloaded with 5 songs. There is a funny story behind the project',
+    tag: '#webpage',
+    techs: 'JavaScript',
+  },
 
-    {
-        id: 1,
-        link: "https://melvinmanni.github.io/ionic-cgpa-calculator/",
-        src: "./assets/images/projects/cgpa-calculator.png",
-        title: "CGPA Calculator",
-        description: "I have always found it a bit tedious calculating CGPA manually so i thought of a way to use what i love doing to make things a little easier for me. :)",
-        tag: "Web-Application Cordova",
-        techs: "Jquery Ionic",
-    },
+  {
+    id: 8,
+    src: './assets/images/projects/tv-mood.png',
+    link: 'https://tv-mood.netlify.app/',
+    title: 'Tv-mood',
+    description:
+      'Movie description/review web application template built with create-react-app',
+    tag: '#web-application',
+    techs: 'Reactjs Redux Axios',
+  },
 
-    {
-        id: 7,
-        src:"./assets/images/projects/HugoFx.png",
-        link: "https://melvinmanni.github.io/HUGO-s-Way/index.html",
-        title: "HUGOfx",
-        description: "HUGOfx is a dashboard template site, it incoporates editable table and fully responsive and comfy feel for both mobile and desktop",
-        tag: "#Website #Dashboard",
-        techs: "JavaScript datatable.js",
-    },
-
-    {
-        id: 8,
-        src:"./assets/images/projects/music-player.png",
-        link: "https://melvinmanni.github.io/music-player",
-        title: "Web Mp3 Player",
-        description: "This is a static mp3 player site preloaded with 5 songs. There is a funny story behind the project",
-        tag: "#webpage",
-        techs: "JavaScript",
-    },
-
-    
-    {
-        id: 8,
-        src:"./assets/images/projects/tv-mood.png",
-        link: "https://tv-mood.netlify.app/",
-        title: "Tv-mood",
-        description: "Movie description/review web application template built with create-react-app",
-        tag: "#web-application",
-        techs: "Reactjs Redux Axios",
-    },
-
-
-]
-
-
-
+  {
+    id: 9,
+    src: './assets/images/projects/collage.png',
+    link: 'https://cool-collage.netlify.app/',
+    title: 'Cool Collage Maker',
+    description:
+      'A SaaS web app that allows you mix and edit multiple pictures too have a cool fresh look',
+    tag: '#web-SaaS',
+    techs: 'Javascript Fabric.js',
+  },
+];
 
 // {
-//      id: , 
+//      id: ,
 //     link:"",
 //     src:"",
 //     title:"",
@@ -88,7 +96,6 @@ let projectAr = [
 //     tag:"",
 //     techs:"",
 // }
-
 
 let projectArr = projectAr.reverse();
 
